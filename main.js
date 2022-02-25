20be170d0d37aa03ac01685ad07515261d04973f
ClassicMC <classicmclive@gmail.com>
2022-02-25 12:04:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ const c = canvas.getContext("2d");
 var images = {fish:document.getElementById("fish"),ocn:document.getElementById("ocn"),fr:document.getElementById("fr")};
 var project = {titleY:0,time:100,key:"right"};
 var p = {x:100,y:100};
+var pt = {x:100,y:200};
 
 let leftKeyPresed = false;
 let rightKeyPressed = false;
 let upKeyPressed = false;
 let downKeyPressed = false;
+//Player 2
+let aKeyPressed = false;
+let dKeyPressed = false;
+let wKeyPressed = false;
+let sKeyPressed = false;
 class Fish{
     draw(x,y){
         if(project.key == "right"){
@@ -46,6 +52,19 @@ function keyPressed(evt){
     if(evt.keyCode == 40){
         downKeyPressed = true;
     }
+    //Player 2
+    if(evt.keyCode == 87){
+        wKeyPressed = true;
+    }
+    if(evt.keyCode == 83){
+        sKeyPressed = true;
+    }
+    if(evt.keyCode == 65){
+        aKeyPressed = true;
+    }
+    if(evt.keyCode == 68){
+        dKeyPressed = true;
+    }
 
 }
 function keyReleased(evt){
@@ -61,16 +80,28 @@ function keyReleased(evt){
     if(evt.keyCode == 40){
         downKeyPressed = false;
     }
+    if(evt.keyCode == 87){
+        wKeyPressed = false;
+    }
+    if(evt.keyCode == 83){
+        sKeyPressed = false;
+    }
+    if(evt.keyCode == 65){
+        aKeyPressed = false;
+    }
+    if(evt.keyCode == 68){
+        dKeyPressed = false;
+    }
 
 }
 function playerMove(){
     if(leftKeyPressed == true){
         p.x -= 3;
-        project.key = "left";
+        //project.key = "left";
     }
     if(rightKeyPressed == true){
         p.x += 3;
-        project.key = "right";
+        //project.key = "right";
     }
     if(upKeyPressed == true){
         if(p.y >= 73){
@@ -80,16 +111,28 @@ function playerMove(){
     if(downKeyPressed == true){
         p.y += 3;
     }
-}
-document.addEventListener('keydown',function (evt){
-    if(event.keyCode == 68){
-        alert(p.x+"X  Y"+p.y);
+    //Player 2
+    if(wKeyPressed){
+        if(pt.y >= 73){
+            pt.y -= 3;
+        }
     }
-});
+    if(sKeyPressed){
+        pt.y += 3;
+    }
+    if(aKeyPressed){
+        pt.x -= 3
+    }
+    if(dKeyPressed){
+        pt.x += 3;
+    }
+}
 const fish = new Fish();
+const fishy = new Fish();
 function redraw(){
     clear();
     fish.draw(p.x,p.y);
+    fishy.draw(pt.x,pt.y);
     ocean();
 }
 redraw();
@@ -97,13 +140,14 @@ window.main = function (){
     window.requestAnimationFrame(main);
     redraw();
     p.y += 1;
+    pt.y +=1;
     document.addEventListener('keydown',keyPressed);
     document.addEventListener('keyup',keyReleased);
     project.titleY += Math.sin(0+(project.time*-0.5)*0.5);
     project.time += 0.5;
     playerMove();
     redraw();
-    if(p.y >= 433){
+    if(p.y >= 433||pt.y >= 433){
         alert("U ded mate");
     }
 }
